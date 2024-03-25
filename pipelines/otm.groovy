a0ea176acb84f053a4d9b8897c8b140311ec77b8
adamsiedlecki <adamsiedlec@gmail.com>
2024-03-25 23:07:49 +0100

    init

--- pipelines/otm.groovy
+++ pipelines/otm.groovy
@@ -1,41 +1,33 @@
-pipeline {
-    agent any
+node(){
 
-    stages {
-        stage('Checkout') {
-            steps {
-                // Krok pobierania projektu z GitHuba
-                script {
-                    git 'https://github.com/adamsiedlecki/OTM.git'
-                }
-            }
-        }
-
-        stage('Build') {
-            steps {
-                // Krok budowania projektu Maven
-                script {
-                    sh "mvn clean package"
-                }
-            }
-        }
-
-        stage('Run Surefire Tests') {
-            steps {
-                // Krok uruchamiający testy Surefire
-                script {
-                    sh "mvn surefire:test"
-                }
-            }
-        }
-
-        stage('Run Failsafe Tests') {
-            steps {
-                // Krok uruchamiający testy Failsafe
-                script {
-                    sh "mvn failsafe:integration-test"
-                }
+    stage('Code Checkout'){
+        checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'GitHubCreds', url: 'https://github.com/adamsiedlecki/OTM.git']])
+    }
+    stage('Build') {
+        steps {
+            // Krok budowania projektu Maven
+            script {
+                sh "mvn clean package"
             }
         }
     }
+    stage('Run Surefire Tests') {
+        steps {
+            // Krok uruchamiający testy Surefire
+            script {
+                sh "mvn surefire:test"
+            }
+        }
+    }
+
+    stage('Run Failsafe Tests') {
+        steps {
+            // Krok uruchamiający testy Failsafe
+            script {
+                sh "mvn failsafe:integration-test"
+            }
+        }
+    }
+
+
 }
